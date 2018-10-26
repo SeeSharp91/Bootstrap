54c36676d0cc838c9ee800d9a42c1569409cf6ed
Micha Rijsmus <1759213rijsmus@zuyd.nl>
2018-10-26 18:13:28 +0200

    Extra's

--- Bootstrap/Scripts/bootstrap.js
+++ Bootstrap/Scripts/bootstrap.js
@@ -2374,4 +2374,14 @@ if (typeof jQuery === 'undefined') {
     })
   })
 
+    /* Set the width of the side navigation to 250px */
+    function openNav() {
+        document.getElementById("mySidenav").style.width = "250px";
+    }
+
+    /* Set the width of the side navigation to 0 */
+    function closeNav() {
+        document.getElementById("mySidenav").style.width = "0";
+    } 
+
 }(jQuery);
